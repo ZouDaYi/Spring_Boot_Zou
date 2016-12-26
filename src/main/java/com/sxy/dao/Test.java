e5c70298a0df509b492c8b4debde1a999735868d
dayi <2850208202@qq.com>
2016-12-26 15:47:53 +0800

    update

---
+++ src/main/java/com/sxy/dao/Test.java
@@ -0,0 +1,7 @@
+package com.sxy.dao;
+
+/**
+ * Created by Administrator on 2016/12/26.
+ */
+public class Test {
+}
---
+++ src/main/java/com/sxy/daoImpl/TestImpl.java
@@ -0,0 +1,7 @@
+package com.sxy.daoImpl;
+
+/**
+ * Created by Administrator on 2016/12/26.
+ */
+public class TestImpl {
+}
