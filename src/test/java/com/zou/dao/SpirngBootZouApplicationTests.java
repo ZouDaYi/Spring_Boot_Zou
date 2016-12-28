384090cd467b9f4b02f8e3cef17e1d13cdd1e745
dayi <2850208202@qq.com>
2016-12-28 14:45:25 +0800

    增加了多数据源配置

--- src/test/java/com/zou/dao/SpirngBootZouApplicationTests.java
+++ src/test/java/com/zou/dao/SpirngBootZouApplicationTests.java
@@ -1,8 +1,12 @@
 package com.zou.dao;
 
 import com.zou.bean.TestBean;
-import com.zou.bean.User;
-import com.zou.bean.UserRepository;
+//import com.zou.bean.User;
+//import com.zou.bean.UserRepository;
+import com.zou.bean.p.User;
+import com.zou.bean.p.UserRepository;
+import com.zou.bean.s.Message;
+import com.zou.bean.s.MessageRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,15 +29,39 @@ public class SpirngBootZouApplicationTests {
 //
 //		System.out.println(testBean.getTempInt()+testBean.getTempLong()+testBean.getTempStrng());
 //	}
+//    @Autowired
+//    private UserRepository userRepository;//数据访问
+//
+//
+//    @Autowired
+//    private CacheManager cacheManager;
+
+    /**
+     * 测试多数据源
+     */
     @Autowired
-    private UserRepository userRepository;//数据访问
-
-
+    private UserRepository userRepository;
     @Autowired
-    private CacheManager cacheManager;
-
+    private MessageRepository messageRepository;
     @Test
     public void test() throws Exception {
+        userRepository.save(new User("aaa", 10));
+        userRepository.save(new User("bbb", 20));
+        userRepository.save(new User("ccc", 30));
+        userRepository.save(new User("ddd", 40));
+        userRepository.save(new User("eee", 50));
+        Assert.assertEquals(5, userRepository.findAll().size());
+        messageRepository.save(new Message("o1", "aaaaaaaaaa"));
+        messageRepository.save(new Message("o2", "bbbbbbbbbb"));
+        messageRepository.save(new Message("o3", "cccccccccc"));
+        Assert.assertEquals(3, messageRepository.findAll().size());
+    }
+
+
+
+
+//    @Test
+//    public void test() throws Exception {
         // 创建10条记录
 //        userRepository.save(new User("AAA", 10));
 //        userRepository.save(new User("BBB", 20));
@@ -57,13 +85,13 @@ public class SpirngBootZouApplicationTests {
 //        userRepository.delete(userRepository.findByName("AAA"));
 //        // 测试findAll, 查询所有记录, 验证上面的删除是否成功
 //        Assert.assertEquals(9, userRepository.findAll().size());
-    }
+//    }
 
-    @Test
-    public void testCache() throws Exception {
-        User u1 = userRepository.findUser("BBB");
-        System.out.println("第一次查询：" + u1.getAge());
-        User u2 = userRepository.findUser("BBB");
-        System.out.println("第二次查询：" + u2.getAge());
-    }
+//    @Test
+//    public void testCache() throws Exception {
+//        User u1 = userRepository.findUser("BBB");
+//        System.out.println("第一次查询：" + u1.getAge());
+//        User u2 = userRepository.findUser("BBB");
+//        System.out.println("第二次查询：" + u2.getAge());
+//    }
     }
